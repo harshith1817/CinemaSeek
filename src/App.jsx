e5f58b9696eee50000acf074a0afd8fe14e9cd9a
HARSHITH CHINTAKINDI <chintakindibhargav2016@gmail.com>
2024-10-15 01:23:43 +0530

    Background Image Changed

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,32 @@ import './App.css';
 
 
 const Background = styled.div`
-  
+  display: flex;
+  flex-direction: column; 
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 85vh; 
 `;
 
+const SearchBar=styled.input`
+width:60%;
+height:2.5rem;
+background-color:white;
+border-radius:0.75rem;
+text-align:center;
+font-size:1rem;
+border:none;
+`
+const SearchButton=styled.button`
+width:10%;
+height:1.5rem;
+background-color:blue;
+border-radius:0.5rem;
+border:none;
+margin-top:0.5rem;
+cursor: pointer;
+`
 
 
 
@@ -19,7 +42,9 @@ function App() {
     return (
         <Router>
             <Background>
-              <img src="./Background.jpg" alt="background" className='bg'/>
+              <img src="https://raw.githubusercontent.com/harshith1817/CinemaSeek/refs/heads/main/src/assets/Background.jpg" alt="background" className='bg'/>
+                <SearchBar type="text" placeholder='Search Here'/>
+                <SearchButton>Search</SearchButton>
                 <Routes>
                     <Route path='/' element={<Home />} />
                     <Route path='/Saved' element={<Saved />} />
